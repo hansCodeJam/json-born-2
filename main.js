77f702a71a6f64ebaf6eb56e9d5608715b085c5b
phenix1229 <paul.garay@codeimmersives.com>
2019-12-16 21:34:19 -0500

    first 5 tasks done

--- main.js
+++ main.js
@@ -2,29 +2,111 @@ const fs = require('fs')
 
 let index = process.argv[4]
 
-fs.readFile('./users.json',(error,user)=>{
+// fs.readFile('./users.json',(error,user)=>{
 
-    // console.log(JSON.parse( user))
-})
+//     // console.log(JSON.parse( user))
+// })
 
-// console.log(index)
+// // console.log(index)
 
 
-fs.readFile('./users.json',(error,data)=>{
+// fs.readFile('./users.json',(error,data)=>{
     
 
-    let blah = [...JSON.parse(data)]
+//     let blah = [...JSON.parse(data)]
 
     
-     for(let i =0;i<user.length;i++){
+//      for(let i =0;i<user.length;i++){
         
-        console.log(blah[i])
+//         console.log(blah[i])
    
         
-     }
+//      }
    
-    // console.log(JSON.parse(value))
-})
+//     // console.log(JSON.parse(value))
+// })
 
 
+const getUsers = () => {
+    fs.readFile('users.json', function(err, data){
+    if (err){
+        console.log(err);
+    } else {
+        let users = JSON.parse(data);
+        console.log(users)
+    }
+    })
+}
 
+getUsers()
+
+const getUser = (i) => {
+    fs.readFile('users.json', function(err, data){
+        if (err){
+            console.log(err);
+        } else {
+            let users = [...JSON.parse(data)];
+            console.log(users[i]);
+}})}
+
+getUser(2)
+
+const getFriends = (i) => {
+    fs.readFile('users.json', function(err, data){
+        if (err){
+            console.log(err);
+        } else {
+            let users = [...JSON.parse(data)];
+            console.log(users[i].friends);
+        }})
+}
+
+getFriends(1)
+
+const postUser = (name, age, eyeColor) => {
+    fs.readFile('users.json', function(err, data){
+        if (err){
+            console.log(err);
+        } else {
+            let users = [...JSON.parse(data)];
+            const user = {
+                "index": users.length,
+                "age": age,
+                "eyeColor": eyeColor,
+                "name": name
+            };
+            users.push(user);
+        
+            const data2 = JSON.stringify(users, null, 2);
+            fs.writeFile('users.json', data2, function(){
+                console.log('Added new user');
+            })
+        }
+    })   
+    console.log(getUsers());
+}
+
+postUser("Alice Um", "18", "green")
+
+const postNewFriend = (index, name) => {
+    fs.readFile('users.json', function(err, data){
+        if (err){
+            console.log(err);
+        } else {
+            let users = [...JSON.parse(data)];
+            if (!users[index]["friends"]){
+                users[index]["friends"] = [];
+            }
+            const newFriend = {
+                "id": users[index].friends.length,
+                "name": name
+            };
+            users[index].friends.push(newFriend);
+            const data2 = JSON.stringify(users, null, 2);
+            fs.writeFile('users.json', data2, function(){
+            })
+        }
+    })   
+}
+
+postNewFriend(10, "Mad Hatter")
